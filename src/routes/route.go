8617cc1ba9893bbdf754df478f8a02c4b8768b1e
Mark Parker <marksadpepe@gmail.com>
2024-04-28 18:54:28 +0300

    formatted code

--- src/routes/route.go
+++ src/routes/route.go
@@ -1,41 +1,41 @@
 package routes
 
 import (
-  "fmt"
+	"fmt"
 
-  "github.com/labstack/echo/v4"
-  "gitlab.com/croc3/umbrellacorptask/src/services"
-  "gitlab.com/croc3/umbrellacorptask/src/controllers"
-  mdw "gitlab.com/croc3/umbrellacorptask/src/middlewares"
+	"github.com/labstack/echo/v4"
+	"gitlab.com/croc3/umbrellacorptask/src/controllers"
+	mdw "gitlab.com/croc3/umbrellacorptask/src/middlewares"
+	"gitlab.com/croc3/umbrellacorptask/src/services"
 )
 
 type Route struct {
-  s *services.DateService
-  c controllers.Controller
-  echo *echo.Echo
+	s    *services.DateService
+	c    controllers.Controller
+	echo *echo.Echo
 }
 
 func NewRoute() (*Route, error) {
-  r := &Route{}
+	r := &Route{}
 
-  r.s = services.NewDateService()
-  r.c = controllers.NewController(r.s)
-  r.echo = echo.New()
+	r.s = services.NewDateService()
+	r.c = controllers.NewController(r.s)
+	r.echo = echo.New()
 
-  r.echo.Use(mdw.IsUserAdmin)
+	r.echo.Use(mdw.IsUserAdmin)
 
-  r.echo.GET("/days", r.c.HandleRequest)
+	r.echo.GET("/days", r.c.HandleRequest)
 
-  return r, nil
+	return r, nil
 }
 
 func (r *Route) Process() error {
-  fmt.Printf("Server is running on 4000 port")
+	fmt.Printf("Server is running on 4000 port")
 
-  err := r.echo.Start(":4000")
-  if err != nil {
-    return fmt.Errorf("failed to start http server: %w", err)
-  }
+	err := r.echo.Start(":4000")
+	if err != nil {
+		return fmt.Errorf("failed to start http server: %w", err)
+	}
 
-  return nil
+	return nil
 }
